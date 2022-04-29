c24b4d3424979d0dbf73e87203503a49e8aeefe7
Godwill Tetah <godwilltetah@gmail.com>
2022-04-29 20:16:13 +0100

    set up DB

--- schoolapp/server.js
+++ schoolapp/server.js
@@ -1,11 +1,29 @@
 const express = require ('express');
-const app = express ();
-const port = 8000;
+const app = express (); //express app init
+const port = 8000; //port number
+const mongoose = require ('mongoose');
 
+//connect to mongoDB Database
+mongoose
+        .connect ('mongodb://localhost:27017/schoolTestApp', {useNewUrlParser: true, useUnifiedTopology: true})  
+        .then(() => console.log('MongoDB Connected'))
+         .catch(err => console.log(err));
+
+//Schema
+const 
+        
+
+//setting up routes
 app.get ('/', (req, res)=>{
     res.send('Welcome to home page')
 });
 
-app.listen (port, ()=>{
+//404 Page Handler
+app.use((req, res)=>{
+    res.status(404).send(`Page not found 😉`);
+})
+
+//listening on port 8000
+app.listen (port, ()=>{ 
     console.log(`Server up and running on port ${port}`);
 })
