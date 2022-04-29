919180e6148de3db17b6a3051b6eec04e5f4c376
Godwill Tetah <godwilltetah@gmail.com>
2022-04-29 21:16:35 +0100

    added a database

--- schoolapp/server.js
+++ schoolapp/server.js
@@ -10,13 +10,72 @@ mongoose
          .catch(err => console.log(err));
 
 //Schema
-const 
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'this field is required'
+    },
+
+    gender: {
+        type: String,
+        required: false
+    },
+
+    department: {
+        type: String,
+        required: 'required'
+    },
+
+    schoolID: {
+        type: String,
+        required: true
+    },
+
+    contactInfo: {
+        tel: [Number],
+        email: [String],
+        address: {
+            city: String,
+            street: String,
+            houseNumber: String
+        }
+    }   
+
+});
+//student model
+const student = mongoose.model('student', studentSchema);
         
 
 //setting up routes
 app.get ('/', (req, res)=>{
     res.send('Welcome to home page')
 });
+
+app.get ('/students', (req, res)=>{
+    //
+})
+
+//new student post route
+app.post ('/new-student', (req, res)=>{
+    const {name, gender, department, schoolID, contactInfo:{tel, email, address:{city, street, houseNumber}}} = req.body;
+    const newStudent = new student({
+        name,
+        gender, 
+        department, 
+        schoolID, 
+        tel, email, city, street, houseNumber
+    });
+
+    newStudent.save((err, doc)=>{
+        if(!err){
+            console.log(`new student added: ${doc}`);
+        } else {
+            console.log(err);
+        }
+    })
+
+});
+
 
 //404 Page Handler
 app.use((req, res)=>{
